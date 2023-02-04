43d97b6d87d24dab5b1216979b7058b16bc029a4
Jon Deaves <hi@jondeaves.me>
2023-02-03 23:06:42 -0500

    feat: first-person gamepad controls

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,28 +9,66 @@ public class Launcher : MonoBehaviour
     [SerializeField]
     private GameObject m_Potato;
 
+    [Tooltip("The object that the Camera looks at")]
+    [SerializeField]
+    private GameObject m_Eyes;
+
+    [Tooltip("The object that the Potato is released from")]
+    [SerializeField]
+    private GameObject m_Hand;
+
+    [Tooltip("How fast the player rotates")]
+    [SerializeField]
+    private float m_TurnSpeed= 200f;
+
+    [Tooltip("How fast the players head moves up and down")]
+    [SerializeField]
+    private float m_LookSpeed = 10f;
+
     private GameObject m_PotatoInstance;
+    private Rigidbody m_Rigidbody;
+
+    private float m_EyesDefaultY;
+    private float m_EyeMovementRange = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_Rigidbody= GetComponent<Rigidbody>();
+        m_EyesDefaultY = m_Eyes.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateInput();
         if (m_PotatoInstance != null && !m_PotatoInstance.activeInHierarchy)
         {
             m_PotatoInstance = null;
         }
 
-        if (!m_PotatoInstance && Input.GetKeyUp(KeyCode.Space))
+        if (!m_PotatoInstance && Input.GetAxis("Fire1") > 0f)
         {
-            Vector3 newPosition = transform.position + new Vector3(0, 0.15f, 1.15f);
-            Quaternion newRotation = transform.rotation;
+            m_PotatoInstance = GameObject.Instantiate(m_Potato, m_Hand.transform.position, Quaternion.identity);
+            m_PotatoInstance.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.forward * 400f);
+        }
+    }
 
-            m_PotatoInstance = GameObject.Instantiate(m_Potato, newPosition, newRotation);
-            m_PotatoInstance.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 400));
+    private void UpdateInput()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            m_Rigidbody.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal") * Time.deltaTime * m_TurnSpeed, Vector3.up);
+        }
+
+        if (Input.GetAxis("Vertical") != 0) {
+            // Clamp to default +/- m_EyeMovementRange
+            float updatedY = Mathf.Clamp(m_Eyes.transform.position.y + (0.1f * Input.GetAxis("Vertical") * Time.deltaTime * m_LookSpeed), m_EyesDefaultY - m_EyeMovementRange, m_EyesDefaultY + m_EyeMovementRange);
+
+            m_Eyes.transform.position = new Vector3(
+                m_Eyes.transform.position.x,
+                updatedY,
+                m_Eyes.transform.position.z);
         }
     }
 }
--- Assets/Scripts/Potato.cs
+++ Assets/Scripts/Potato.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Potato : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("The number of seconds before the potato is considered dead")]
+    private float m_TTL = 3f;
+
+    private float m_TimeAlive = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_TimeAlive = 0f;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_TimeAlive += Time.deltaTime;
+
+        if (m_TimeAlive >= m_TTL)
+        {
+            Destroy(gameObject);
+        }
     }
 }
